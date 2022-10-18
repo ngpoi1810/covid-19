87e2e90a7ffb05e4f6ca53ef7a68ce9c8643a4e4
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2022-10-18 16:09:01 +0700

    first commit

---
+++ components/Highlight/index.js
@@ -0,0 +1,5 @@
+function Highlight() {
+    return <div>This is Highlight</div>
+}
+
+export default Highlight;
---
+++ components/Summary/index.js
@@ -0,0 +1,5 @@
+function Summary() {
+    return <div>This is Summary</div>
+}
+
+export default Summary;
